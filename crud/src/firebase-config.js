2318686435101b747581dad1f69f9ca6c0f9932e
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-10-13 19:37:43 +0530

    init

---
+++ crud/src/firebase-config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBmX1PuEFr9-v6MiZgf8m62gbeKVUio7zo",
+    authDomain: "crud-c475e.firebaseapp.com",
+    projectId: "crud-c475e",
+    storageBucket: "crud-c475e.appspot.com",
+    messagingSenderId: "127750032293",
+    appId: "1:127750032293:web:122958263f0444d127764c",
+    measurementId: "G-96LBNYX22C"
+  };
