f952f457727f0b5f5524fc9e029b7b6df9b0c334
MurtazaBagwala07 <murtazabag786@gmail.com>
2021-10-14 18:40:55 +0530

    completed

--- crud/src/App.js
+++ crud/src/App.js
@@ -21,7 +21,18 @@ function App() {
   const usersCollectionRef = collection(db,"users")
 
   const createUser = async () =>{
-    await addDoc(usersCollectionRef, {name: newName,age: newAge})
+    await addDoc(usersCollectionRef, {name: newName,age: Number(newAge)})
+  }
+
+  const updateUser = async (id,age) =>{
+    const userDoc = doc(db,"users",id)
+    const newFields = {age: age+1}
+    await updateDoc(userDoc,newFields)
+  }
+
+  const deleteUser =  async (id) =>{
+    const userDoc = doc(db,"users",id);
+    deleteDoc(userDoc);
   }
 
   useEffect(() => {
@@ -52,6 +63,8 @@ function App() {
             {" "}
           <h1>Name : {user.name}</h1>
           <h1>Name : {user.age}</h1>
+          <button onClick={()=>{updateUser(user.id,user.age)}}>increase age</button>
+          <button onClick={()=>{deleteUser(user.id)}}>Delete User</button>
           </div>
         );
       })}
